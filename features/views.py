f70d95be49d15e57c3825699a4b0dd2f7362dba8
Darren Tanner <darrentan@icloud.com>
2019-09-21 20:59:48 -0600

    Added the REST API that Vue.js will connect to in order to build the UI.

--- features/views.py
+++ features/views.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from rest_framework import viewsets
+from features.serializers import ClientSerializer, ProjectSerializer, FeatureRequestSerializer
+from features.models import Client, FeatureRequest, Project
 
-from django.shortcuts import render
 
-# Create your views here.
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class FeatureRequestViewSet(viewsets.ModelViewSet):
+    queryset = FeatureRequest.objects.all()
+    serializer_class = FeatureRequestSerializer
