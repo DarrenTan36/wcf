c0a215412851b66ca9e22dd22727918e93fdb2a6
Darren Tanner <darrentan@icloud.com>
2019-09-21 19:07:13 -0600

    Created the database model for the features request application and performed both makemigrations and migrated the database.

--- features/models.py
+++ features/models.py
@@ -3,4 +3,56 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    """
+    Client model to be utilized for populating the
+    select list in the view.
+    """
+    name = models.CharField(max_length=64, default='', blank=False, null=False)
+    contact = models.CharField(max_length=64, blank=True, null=True)
+    street = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=64, blank=True, null=True)
+    state = models.CharField(max_length=128, blank=True, null=True)
+    zip = models.CharField(max_length=24, blank=True, null=True)
+    phone = models.CharField(max_length=11, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Clients'
+
+
+class Project(models.Model):
+    """
+    The Project model is to identify the software that the client
+    is requesting the feature for.
+    """
+    name = models.CharField(max_length=255, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Projects'
+
+
+class FeatureRequest(models.Model):
+    title = models.CharField(max_length=48, blank=False, null=False)
+    description = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    target_date = models.DateField(blank=True, null=True)
+    priority = models.IntegerField(default=0)
+    project = models.ForeignKey(Project, related_name='requests', null=True, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, related_name='requests', null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        unique_together = ['priority', 'project']
+        ordering = ['project', 'priority']
+        verbose_name = 'Feature Requests'
--- wcf/settings.py
+++ wcf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1g$kut)b@4)$h+on!3$ys-7^6b-yq84uv39*1$b-s(7%p)!z8g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'features',
 ]
 
 MIDDLEWARE = [
